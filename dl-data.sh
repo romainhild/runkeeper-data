eb8a59c18a4eb5317cfabaeb888beaaf9c2c3394
romainhild <romain.hild@me.com>
2020-04-16 21:13:48 +0200

    implement cache busting

--- dl-data.sh
+++ dl-data.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+rm *html
+rm *js
+rm *php
+
 wget $1
 unzip -o *.zip -d runkeeper-data
 rm *.zip
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate jupyter
 python stats-runkeeper.py
+
+d=`date +%F`
+mv runkeeper-data/cardioActivities.csv runkeeper-data/cardioActivities-$d.csv
+sed -e 's/\.html/-'$d'.html/g' index.php_tpl > index.php
+sed -i -e 's/script.js/script-'$d'.js/g' index.php
+for i in *.html
+do
+    mv $i ${i/.html/-`date +%F`.html}
+done
+sed -e 's/cardioActivities.csv/cardioActivities-'$d'.csv/' script.js_tpl > script-$d.js
