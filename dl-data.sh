cee5f8320cd0df95f85165f6ae6160a12bb276fc
romainhild <romain.hild@me.com>
2020-04-16 23:35:54 +0200

    rm more precise filename

--- dl-data.sh
+++ dl-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 rm *html
-rm *js
-rm *php
+rm script-*js
+rm index.php
 
 wget $1
 unzip -o *.zip -d runkeeper-data
@@ -13,10 +13,10 @@ python stats-runkeeper.py
 
 d=`date +%F`
 mv runkeeper-data/cardioActivities.csv runkeeper-data/cardioActivities-$d.csv
-sed -e 's/\.html/-'$d'.html/g' index.php_tpl > index.php
+sed -e 's/\.html/-'$d'.html/g' template_index.php > index.php
 sed -i -e 's/script.js/script-'$d'.js/g' index.php
 for i in *.html
 do
     mv $i ${i/.html/-`date +%F`.html}
 done
-sed -e 's/cardioActivities.csv/cardioActivities-'$d'.csv/' script.js_tpl > script-$d.js
+sed -e 's/cardioActivities.csv/cardioActivities-'$d'.csv/' template_script.js > script-$d.js
