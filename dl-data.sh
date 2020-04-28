a20cdaeca8ab480e07de24288cc1709dd210fc15
romainhild <romain.hild@me.com>
2020-04-29 00:31:16 +0200

    update dl-data script

--- dl-data.sh
+++ dl-data.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-rm *html
-rm script-*js
-rm index.php
-
-wget $1
-unzip -o *.zip -d runkeeper-data
-rm *.zip
-source ~/miniconda3/etc/profile.d/conda.sh
+if [ $# -eq 1 ]
+then
+    wget $1
+    unzip -o *.zip -d runkeeper-data-export
+    sed -i '' -e '1r runkeeper-data-export/cardioActivities.csv' -e '2,$!d' runkeeper-data/cardioActivities.csv
+    mv runkeeper-data-export/*.gpx runkeeper-data/
+    rm -rf *.zip runkeeper-data-export
+fi
+# folder where anacondo is installed with the jupyter env
+# source ~/miniconda3/etc/profile.d/conda.sh
+source ~/Documents/anaconda3/etc/profile.d/conda.sh
 conda activate jupyter
-python stats-runkeeper.py
-
-d=`date +%F`
-mv runkeeper-data/cardioActivities.csv runkeeper-data/cardioActivities-$d.csv
-sed -e 's/\.html/-'$d'.html/g' template_index.php > index.php
-sed -i -e 's/script.js/script-'$d'.js/g' index.php
-for i in *.html
-do
-    mv $i ${i/.html/-`date +%F`.html}
-done
-sed -e 's/cardioActivities.csv/cardioActivities-'$d'.csv/' template_script.js > script-$d.js
+python app.py
